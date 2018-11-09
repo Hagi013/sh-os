a250aa8274d5803eb32f8f1d1e0cddc85b627850
Haaagiii <generativepassion@gmail.com>
2018-11-10 07:49:28 +0900

    add rust kernel code

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,7 +1,27 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![feature(lang_items)]
+#![feature(start)]
+#![no_main]
+#![feature(no_std)]
+#![no_std]
+#![feature(asm)]
+
+#[no_mangle]
+fn hlt() {
+    unsafe {
+        asm!("htl");
     }
 }
+
+#![no_mangle]
+#[start]
+pub extern fn init_os() {
+    loop {
+        hlt()
+    }
+}
+
+#[lang = "eh_personality"]
+extern fn en_personality() {}
+
+#[lang = "panic_fmt"]
+extern fn panic_fmt() -> ! { loop{} }
